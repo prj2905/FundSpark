21737a3b12477c47c7321c08fe7fe048884be01e
Pranav <pranavjoshi935@gmail.com>
2025-07-22 00:20:30 +0530

    CORS for chatbot changed

--- src/app/chatbot/main.py
+++ src/app/chatbot/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["https://fund-spark-wbv7.vercel.app/", "http://localhost:3000"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
